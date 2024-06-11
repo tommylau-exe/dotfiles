291dadce9b66452e6fcd9bbde13a079ff9db3f00
Thomas Lauerman <tom@lauerman.dev>
2024-06-10 22:46:14 -0700

    fix: erroneous space in lazy.nvim path

--- nvim/lua/tom/lazy.lua
+++ nvim/lua/tom/lazy.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy /lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
         "git",
