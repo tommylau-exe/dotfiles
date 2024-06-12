84d99f51f3e8e45413c92bde112719fa08d40c57
Thomas Lauerman <tom@lauerman.dev>
2024-06-12 16:38:02 -0700

    nvim: native copy/paste cliboard remaps

--- nvim/lua/tom/remap.lua
+++ nvim/lua/tom/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
+
+-- yank/paste remaps
+vim.keymap.set("n", "<leader>cy", "\"+y")
+vim.keymap.set("v", "<leader>cy", "\"+y")
+vim.keymap.set("n", "<leader>cp", "\"+p")
